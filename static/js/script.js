9f889e96d68fed69f095dcad99d749fa84529660
wearenifty <david@wearenifty.com>
2013-06-27 03:39:45 -0700

    added nav rollover state

--- static/js/script.js
+++ static/js/script.js
@@ -1,44 +1,34 @@
 /* Author: YOUR NAME HERE
 */
 
-var $container = $('.pure-g-r');
-
 var viewWidth = $('html').width();
 
 $(document).ready(function() {
     window.mySwipe = Swipe(document.getElementById('slider'));
 
+    switchContent(window.location.pathname)
     $('.left-dock .nav li a, .logo').click(function(e) {
         e.preventDefault()
         var href = $(this).attr("href");
 
         history.pushState({}, '', href);
-        $('body').backstretch('destroy');
-        $('body').removeClass().addClass(href.replace('/', ''));
-        if (href == '/') $('body').addClass('home');
+
         $('.content').load(href + " .content>*", function() {
-            showTiles()
+            switchContent(href);
         })
-        $('body.home').backstretch('/images/main-hero_1024.jpg')
+
     })
-    $('.content').width($('html').width() - 375);
 
-    // initialize
-    $container.imagesLoaded( function(){
-        showTiles()
-    });
-
-    $('body.home').backstretch('/images/main-hero_1024.jpg');
 });
 
-var timer;
-$(window).bind('resize', function(){
-    $('.content').width($('html').width() - 375);
-    timer && clearTimeout(timer);
-    timer = setTimeout(function() {
-        $container.masonry('layout');
-    }, 500);
-});
+//var timer;
+//$(window).bind('resize', function(){
+//    $('.content').width($('html').width() - 375);
+//    timer && clearTimeout(timer);
+//    timer = setTimeout(function() {
+//        $container.masonry('layout');
+//    }, 500);
+//});
 
 function showTiles() {
     $('#tiles').css('top', 34);
@@ -51,4 +41,41 @@ function showTiles() {
     });
     $container.masonry('unbindResize');
 
+}
+
+function switchContent(href) {
+    switch(href) {
+        case '/':
+            $('body').removeClass().addClass('home');
+
+            var elem = document.getElementById('mySwipe');
+            window.mySwipe = Swipe(elem, {
+                callback: function(index, element) {
+                    $('.slides-pagination a').removeClass('current');
+                    $('.slides-pagination a').eq(index).addClass('current');
+
+                    loadPage(index);
+                },
+                transitionEnd: function(index, element) {
+
+                }
+            });
+
+            $('#a').backstretch("/images/slide1.jpg");
+            $('#b').backstretch("img/screen3-hero-bg.jpg");
+            $('#c').backstretch("img/new-whitehouse.jpg");
+            break;
+        case '/latifah':
+            $('body').removeClass().addClass('latifah');
+
+            var $container = $('.pure-g-r');
+            $('.content').width($('html').width() - 375);
+
+            // initialize
+            $container.imagesLoaded( function(){
+                showTiles()
+
+            });
+            break;
+    }
 }
