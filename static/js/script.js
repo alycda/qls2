6dbb5e5efbd4675bf562e6874b995360cdafb2e9
wearenifty <david@wearenifty.com>
2013-06-27 01:23:35 -0700

    implemented backbone.js

--- static/js/script.js
+++ static/js/script.js
@@ -8,23 +8,27 @@ var viewWidth = $('html').width();
 $(document).ready(function() {
     window.mySwipe = Swipe(document.getElementById('slider'));
 
+    $('.left-dock .nav li a, .logo').click(function(e) {
+        e.preventDefault()
+        var href = $(this).attr("href");
+
+        history.pushState({}, '', href);
+        $('body').backstretch('destroy');
+        $('body').removeClass().addClass(href.replace('/', ''));
+        if (href == '/') $('body').addClass('home');
+        $('.content').load(href + " .content>*", function() {
+            showTiles()
+        })
+        $('body.home').backstretch('/images/main-hero_1024.jpg')
+    })
     $('.content').width($('html').width() - 375);
 
     // initialize
     $container.imagesLoaded( function(){
-        $('#tiles').css('top', 34);
-        $container.masonry({
-            columnWidth: '.tile', //must be the base tile width
-            itemSelector: '.pure-u-1-8',
-            gutterWidth: 0,
-            containerStyle: null,
-            transitionDuration: 0
-        });
-        $container.masonry('unbindResize');
+        showTiles()
     });
 
-
-    $('body.home').backstretch('/images/main-hero_1024.jpg')
+    $('body.home').backstretch('/images/main-hero_1024.jpg');
 });
 
 var timer;
@@ -35,3 +39,16 @@ $(window).bind('resize', function(){
         $container.masonry('layout');
     }, 500);
 });
+
+function showTiles() {
+    $('#tiles').css('top', 34);
+    $container.masonry({
+        columnWidth: '.tile', //must be the base tile width
+        itemSelector: '.pure-u-1-8',
+        gutterWidth: 0,
+        containerStyle: null,
+        transitionDuration: 0
+    });
+    $container.masonry('unbindResize');
+
+}
