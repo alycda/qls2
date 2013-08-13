553456306bc4c14b1c0babc72f3124d832f4dca6
wearenifty <david@wearenifty.com>
2013-08-12 23:18:30 -0700

    client-side json tvguide

---
+++ static/js/tvguide.js
@@ -0,0 +1,498 @@
+var listings = {
+    "alabama": {
+        "birmingham": {
+            "station": ["wbrc"],
+            "network": ["fox"],
+            "time": ["3pm", "1am"]
+
+        },
+        "dothan": {
+            "station": ["wdfx"],
+            "network": ["fox"],
+            "time": ["3pm"]
+        },
+        "huntsville": {
+            "station": ["waff"],
+            "network": ["nbc"],
+            "time": ["2pm"]
+        },
+        "mobile": {
+            "station": ["wear"],
+            "network": ["abc"],
+            "time": ["2pm"]
+        },
+        "montgomery": {
+            "station": ["wncf"],
+            "network": ["abc"],
+            "time": ["11am"]
+        }
+    },
+    "arizona": {
+        "phoenix": {
+            "station": ["ktvk"],
+            "network": ["ind"],
+            "time": ["2pm"]
+        },
+        "tucson": {
+            "station": ["kmsb"],
+            "network": ["fox"],
+            "time": ["10am"]
+        },
+        "yuma": {
+            "station": ["kecy"],
+            "network": ["fox"],
+            "time": ["3pm", "4am"]
+        }
+    },
+    "arkansas": {
+        "fortsmith": {
+            "station": ["knwa"],
+            "network": ["nbc"],
+            "time": ["11am"]
+        },
+        "littlerock": {
+            "station": ["klrt"],
+            "network": ["fox"],
+            "time": ["2pm"]
+        }
+    },
+    "california": {
+        "bakersfield": {
+            "station": ["kget"],
+            "network": ["nbc"],
+            "time": ["2pm"]
+        },
+        "chico": {
+            "station": ["knvn"],
+            "network": ["nbc"],
+            "time": ["2pm"]
+        },
+        "eureka": {
+            "station": ["kiem"],
+            "network": ["nbc"],
+            "time": ["2pm"]
+        },
+        "fresno": {
+            "station": ["ksee"],
+            "network": ["nbc"],
+            "time": ["2pm"]
+        },
+        "losangeles": {
+            "station": ["kcbs", "kcal"],
+            "network": ["cbs", "ind"],
+            "time": ["2pm", "7pm"]
+        },
+        "monterey": {
+            "station": ["ksbw"],
+            "network": ["abc"],
+            "time": ["9am"]
+        },
+        "palmsprings": {
+            "station": ["kmir", "kpse"],
+            "network": ["nbc", "myn"],
+            "time": ["2pm", "3pm"]
+        },
+        "sacramento": {
+            "station": ["kovr"],
+            "network": ["cbs"],
+            "time": ["2pm"]
+        },
+        "sandiego": {
+            "station": ["kusi"],
+            "network": ["ind"],
+            "time": ["2pm", "8pm"]
+        },
+        "sanfrancisco": {
+            "station": ["kpix", "kbcw"],
+            "network": ["cbs", "cw"],
+            "time": ["2pm", "8am"]
+        },
+        "santabarbara": {
+            "station": ["keyt"],
+            "network": ["abc"],
+            "time": ["2pm"]
+        }
+    },
+    "colorado": {
+        "coloradosprings": {
+            "station": ["kxtu"],
+            "network": ["cw"],
+            "time": ["4pm"]
+        },
+        "denver": {
+            "station": ["kcnc"],
+            "network": ["cbs"],
+            "time": ["3pm"]
+        }
+    },
+    "connecticut": {
+        "hartford": {
+            "station": ["wvit"],
+            "network": ["nbc"],
+            "time": ["2pm"]
+        }
+    },
+    "d.c.": {
+        "washington": {
+            "station": ["wjla"],
+            "network": ["abc"],
+            "time": ["4pm"]
+        }
+    },
+    "florida": {
+        "fort myers": {
+            "station": ["wxcw"],
+            "network": ["cw"],
+            "time": ["2pm"]
+        },
+        "gainesville": {
+            "station": ["wnbw"],
+            "network": ["nbc"],
+            "time": ["2pm"]
+        },
+        "jacksonville": {
+            "station": ["wjxt"],
+            "network": ["ind"],
+            "time": ["3pm"]
+        },
+        "miami": {
+            "station": ["wfor"],
+            "network": ["cbs"],
+            "time": ["9am"]
+        },
+        "orlando": {
+            "station": ["wkmg"],
+            "network": ["cbs"],
+            "time": ["3pm"]
+        },
+        "panama city": {
+            "station": ["wpgx"],
+            "network": ["fox"],
+            "time": ["3pm"]
+        },
+        "tallahassee": {
+            "station": ["wtxl"],
+            "network": ["abc"],
+            "time": ["10am"]
+        },
+        "tampa": {
+            "station": ["wtog"],
+            "network": ["cw"],
+            "time": ["5pm", "10am"]
+        },
+        "westpalmbeach": {
+            "station": ["wptv"],
+            "network": ["nbc"],
+            "time": ["3pm"]
+        }
+    },
+    "georgia": {
+        "albany": {
+            "station": ["walb"],
+            "network": ["nbc"],
+            "time": ["4pm"]
+        },
+        "atlanta": {
+            "station": ["wupa"],
+            "network": ["cw"],
+            "time": ["6pm"]
+        },
+        "augusta": {
+            "station": ["wagt"],
+            "network": ["nbc"],
+            "time": ["4pm"]
+        },
+        "columbus": {
+            "station": ["wtvm"],
+            "network": ["abc"],
+            "time": ["10am"]
+        },
+        "macon": {
+            "station": ["wgxa"],
+            "network": ["fox"],
+            "time": ["4pm"]
+        },
+        "savannah": {
+            "station": ["wtgs"],
+            "network": ["fox"],
+            "time": ["3pm"]
+        }
+    },
+    "hawaii": {
+        "honolulu": {
+            "station": ["kfve"],
+            "network": ["myn"],
+            "time": ["8pm"]
+        }
+    },
+    "idaho": {
+        "boise": {
+            "station": ["kivi"],
+            "network": ["abc"],
+            "time": ["9am"]
+            },
+        "idahofalls": {
+            "station": ["kifi"],
+            "network": ["ab"],
+            "time": ["10am"]
+            },
+        "twinfalls": {
+            "station": ["ksaw"],
+            "network": ["abc"],
+            "time": ["9am"]
+        }
+    },
+    "illinois": {
+        "champaign": {
+            "station": ["wrsp"],
+            "network": ["fox"],
+            "time": ["2pm", "7am"]
+        },
+        "chicago": {
+            "station": ["wbbm"],
+            "network": ["cbs"],
+            "time": ["2pm"]
+        },
+        "chicago": {
+            "station": ["wciu"],
+            "network": ["ind"],
+            "time": ["7pm"]
+        },
+        "rockford": {
+            "station": ["wrex"],
+            "network": ["nbc"],
+            "time": ["11am"]
+        }
+    },
+    "indiana": {
+        "evansville": {
+            "station": ["wfie"],
+            "network": ["nbc"],
+            "time": ["3pm"]
+        },
+        "fortwayne": {
+            "station": ["wise"],
+            "network": ["nbc"],
+            "time": ["2pm"]
+        },
+        "indianapolis": {
+            "station": ["wthr"],
+            "network": ["nbc"],
+            "time": ["11am"]
+        },
+        "southbend": {
+            "station": ["wbnd"],
+            "network": ["abc"],
+            "time": ["10am"]
+        },
+        "terrehaute": {
+            "station": ["wawv"],
+            "network": ["abc"],
+            "time": ["9am"]
+        }
+    },
+    "iowa": {
+        "cedarrapids": {
+            "station": ["kwwl"],
+            "network": ["nbc"],
+            "time": ["3pm"]
+        },
+        "davenport": {
+            "station": ["kwqc"],
+            "network": ["nbc"],
+            "time": ["2pm"]
+        },
+        "desmoines": {
+            "station": ["who"],
+            "network": ["nbc"],
+            "time": ["2pm"]
+        },
+        "masoncity": {
+            "station": ["kttc"],
+            "network": ["nbc"],
+            "time": ["3pm"]
+        },
+        "siouxcity": {
+            "station": ["ktiv"],
+            "network": ["nbc"],
+            "time": ["3pm"]
+        }
+    },
+    "kansas": {
+        "topeka": {
+            "station": ["ktka"],
+            "network": ["abc"],
+            "time": ["11am"]
+        },
+        "wichita": {
+            "station": ["ksnw"],
+            "network": ["nbc"],
+            "time": ["2pm"]
+        }
+    },
+    "kentucky": {
+        "bowlinggreen": {
+            "station": ["wbko"],
+            "network": ["abc"],
+            "time": ["2pm"]
+        },
+        "lexington": {
+            "station": ["wdky"],
+            "network": ["fox"],
+            "time": ["10am"]
+        },
+        "louisville": {
+            "station": ["wave"],
+            "network": ["nbc"],
+            "time": ["4pm"]
+        },
+        "paducah": {
+            "station": ["wdka"],
+            "network": ["myn"],
+            "time": ["3pm"]
+        }
+    },
+    "louisiana": {
+        "batonrouge": {
+            "station": ["wgmb"],
+            "network": ["fox"],
+            "time": ["2pm"]
+        },
+        "lafayette": {
+            "station": ["kadn"],
+            "network": ["fox"],
+            "time": ["3pm"]
+        },
+        "lakecharles": {
+            "station": ["kvhp"],
+            "network": ["fox"],
+            "time": ["3pm"]
+        },
+        "monroe": {
+            "station": ["kaqy"],
+            "network": ["abc"],
+            "time": ["3pm"]
+        },
+        "neworleans": {
+            "station": ["wdsu"],
+            "network": ["nbc"],
+            "time": ["11am"]
+        },
+        "shreveport": {
+            "station": ["ktbs"],
+            "network": ["abc"],
+            "time": ["2pm"]
+        }
+    },
+    "maine": {
+        "portland": {
+            "station": ["wgme"],
+            "network": ["cbs"],
+            "time": ["9am"]
+        }
+    },
+    "maryland": {
+        "baltimore": {
+            "station": ["wbff", "wutb"],
+            "network": ["fox", "myn"],
+            "time": ["1pm", "4pm"]
+        },
+        "salisbury": {
+            "station": ["wmdt"],
+            "network": ["abc"],
+            "time": ["3pm"]
+        }
+    },
+    "massachusetts": {
+        "boston": {
+            "station": ["wbz"],
+            "network": ["cbs"],
+            "time": ["9am"]
+        },
+        "springfield": {
+            "station": ["wggb", "wggb-dt2"],
+            "network": ["abc", "fox"],
+            "time": ["10am", "2pm"]
+        },
+    },
+    "michigan": {
+        "detroit": {
+            "station": ["wwj", "wkbd"],
+            "network": ["cbs", "cw"],
+            "time": ["4pm", "7am"]
+        },
+        "flint": {
+            "station": ["weyi"],
+            "network": ["nbc"],
+            "time": ["2pm"]
+        },
+        "grandrapids": {
+            "station": ["wotv", "wxsp"],
+            "network": ["abc", "myn"],
+            "time": ["9am", "7pm"]
+        },
+        "lansing": {
+            "station": ["wlaj"],
+            "network": ["abc"],
+            "time": ["4pm"]
+        },
+        "duluth": {
+            "station": ["kdlh"],
+            "network": ["cbs"],
+            "time": ["9am"]
+        },
+        "minneapolis": {
+            "station": ["wcco"],
+            "network": ["cbs"],
+            "time": ["9am"]
+        },
+        "rochestermasoncity": {
+            "station": ["kttc"],
+            "network": ["nbc"],
+            "time": ["3pm"]
+        },
+    },
+    "minnesota": {
+        "duluth": {
+            "station": ["kdlh"],
+            "network": ["cbs"],
+            "time": ["9am"]
+        },
+        "minneapolis": {
+            "station": ["wcco"],
+            "network": ["cbs"],
+            "time": ["9am"]
+        },
+        "rochester": {
+            "station": ["kttc"],
+            "network": ["nbc"],
+            "time": ["3pm"]
+        }
+    },"mississippi": {
+        "biloxi": {
+            "station": ["wlox"],
+            "network": ["nbc"],
+            "time": ["3pm"]
+        },
+        "columbus": {
+            "station": ["wlov"],
+            "network": ["fox"],
+            "time": ["3pm"]
+        },
+        "hattiesburg": {
+            "station": ["wdam"],
+            "network": ["nbc"],
+            "time": ["2:30pm"]
+        },
+        "jackson": {
+            "station": ["wapt"],
+            "network": ["abc"],
+            "time": ["11am"]
+        },
+        "meridian": {
+            "station": ["wgbc"],
+            "network": ["nbc"],
+            "time": ["1pm"]
+        }
+    }
+}
